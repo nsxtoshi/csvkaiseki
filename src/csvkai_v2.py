cd6d96f53c2b00c14300915599d0e7b9fde2a5e7
jinnouchi-jri-aws <jinnouchi.toshiyuki@jri.co.jp>
2024-01-15 01:32:57 +0900

    ｖ３

--- src/csvkai_v2.py
+++ src/csvkai_v2.py
@@ -10,7 +10,7 @@ nltk.download('stopwords')
 nltk.download('punkt')
 
 # Streamlitアプリの開始
-st.title("CSVファイル解析アプリｖ２")
+st.title("CSVファイル解析アプリｖ３")
 
 # CSVファイルのアップロード
 uploaded_file = st.file_uploader("CSVファイルをアップロードしてください", type=["csv"])
